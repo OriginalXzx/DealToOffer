a411e88cdeb18faefc58311fef9d462487567409
OriginalXzx <870567305@qq.com>
2017-05-15 22:02:06 +0800

    20170513提交
    Signed-off-by: OriginalXzx <870567305@qq.com>

---
+++ dealtooffer/src/xzx/part2/link/Link.java
@@ -0,0 +1,98 @@
+package xzx.part2.link;
+
+import java.util.Iterator;
+
+public class Link<Item> {
+    private int N;
+    private Node first;
+    private Node last;
+	private class Node{
+		Item item;
+		Node next;
+	}
+	public  boolean isEmpty(){
+		return first == null;
+	}
+	public int size(){
+		return N;
+	}
+	public void push(Item item){//从栈顶添加元素
+		Node oldFirst = first;
+		first = new Node();
+		first.item = item;
+		first.next = oldFirst;
+		N++;
+	}
+	
+	public Item pop(){//从栈顶移除元素
+		Item item = first.item;
+		first = first.next;
+		N--;
+		return item;
+	}
+	
+	public void enqueue(Item item){//从尾部添加元素
+		Node oldLast = last;
+		last.item = item;
+		last.next = null;
+		if(isEmpty()){
+			first = last;
+		}else{
+			oldLast.next = last;
+		}
+		N++;
+	}
+	
+	public Item removeLast(){
+		Item item = last.item;
+		last=null;
+		return item;
+	}
+	
+	public Iterator<Item> iterator(){
+		return new ListIterator();
+	}
+	
+	private class ListIterator implements Iterator<Item>{
+        
+		private Node current = first;
+		@Override
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		@Override
+		public Item next() {
+			Item item = current.item;
+			current = current.next;
+			return item;
+		}
+		public void remove(){}
+		
+	}
+	/**
+	 * 递归倒着打印链表
+	 * @param first
+	 */
+	public void printLink(Node first){
+		if(first == null){
+			System.out.println("链表没东西了！！！");
+		}else {
+			if(first.next != null){
+				printLink(first.next);
+			}
+			System.out.println(first.item);
+		}
+	}
+	public static void main(String[] args) {
+       Link<String> link = new Link<String>();
+       link.push("我");
+       link.push("爱");
+       link.push("中");
+       link.push("国");
+       System.out.println(link.first.item);
+       link.printLink(link.first);
+       
+	}
+
+}
---
+++ dealtooffer/src/xzx/part2/string/ReplaceNull.java
@@ -0,0 +1,10 @@
+package xzx.part2.string;
+
+public class ReplaceNull {
+
+	public static void main(String[] args) {
+       String str = "We are happy.";
+       str = str.replace(" ", "%20");
+       System.out.println(str);
+	}
+}
