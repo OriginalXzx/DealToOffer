e8e4d03a040ac4f7fde65b0e9adf34b9661ad885
OriginalXzx <870567305@qq.com>
2017-06-05 22:07:56 +0800

    今天情况还不错！！！ Signed-off-by: OriginalXzx <870567305@qq.com>

--- dealtooffer/src/xzx/part2/array/Print1ToMaxOfNDigits.java
+++ dealtooffer/src/xzx/part2/array/Print1ToMaxOfNDigits.java
@@ -7,8 +7,8 @@ package xzx.part2.array;
 public class Print1ToMaxOfNDigits {
 
 	public static void main(String[] args) {
-         int n = 2;
-         printToMax1(n);//没有考虑n很大很大的情况
+         int n = 3;
+         printToMax2(n);//没有考虑n很大很大的情况
 	}
 	/**
 	 * 最容易跳进陷阱的解法，很不好,没有考虑大数的问题
@@ -24,5 +24,42 @@ public class Print1ToMaxOfNDigits {
 			System.out.print(j+" ");
 		}
 	}
-
+    /**
+     * 把问题换成数字全排列的解法，字符串是一个简单的，有效的表示大数的方法
+     * @param n
+     */
+	public static void printToMax2(int n){
+		if(n <= 0){
+			return;
+		}
+		char[] number = new char[n];
+		for(int i = 0;i < 10; i++){
+			number[0] = (char) (i + '0');
+			print1ToMax(number,n,0);
+		}
+		
+	}
+	private static void print1ToMax(char[] number, int n, int i) {
+		if(i == n-1){
+			printNumber(number);
+			return;
+		}
+		for(int j = 0;j<10;j++){
+			number[i+1] = (char) (j+'0');
+			print1ToMax(number, n, i+1);
+		}
+	}
+	private static void printNumber(char[] number) {
+		boolean isBeginning0 = true;
+		int nlength = number.length;
+		for (int i = 0; i < nlength; i++) {
+			if(isBeginning0 && number[i] != '0'){
+				isBeginning0 = false;
+			}
+			if(!isBeginning0){
+				System.out.print(number[i]);
+			}
+		}
+		System.out.println();
+	}
 }
